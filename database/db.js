2383dd373c83e6fba07e58972d2c146984288882
dharamveer77 <77dharmveer77@gmail.com>
2024-10-14 16:41:46 +0530

    database updated

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Connection = async (URL) => {
     try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
         console.log('Database Connected Succesfully');
     } catch(error) {
         console.log('Error: ', error.message);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
